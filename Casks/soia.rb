8c373fe6d83eeece2a111bf305c603a84d478597
FengZeng <zengfeng@pm.me>
2026-08-11 20:12:34 +0800

    Update to 0.2.10

--- Casks/soia.rb
+++ Casks/soia.rb
@@ -2,14 +2,14 @@ cask "soia" do
 
   arch arm: "arm64", intel: "x64"
 
-  version "0.2.9"
+  version "0.2.10"
 
-  sha256 arm:   "2fa65b750c7468605e4c61663d5d03b56e6c0ab8676ed1adf1b2234684dde758",
-         intel: "36b16306c9b4207dc24caa9b85d2ca32b85fc569a5bd2e123d1da4be1630da33"
+  sha256 arm:   "9cd01a89e6fbb693c2bdef25e4f3c004528b81e42786759cb4d8f226b6a3516c",
+         intel: "1ce02d08acca9f5604de2c630f8302233fa13a2e00132e8b61986545ac937672"
 
   url "https://github.com/FengZeng/soia/releases/download/v#{version}/Soia-v#{version}-macOS-#{arch}.dmg"
   name "Soia"
-  desc "High-performance media player, with HDR/Dolby Vision, WebDAV/DLNA/SMB streaming, and modern UI"
+  desc "High-performance cross-platform media player with HDR/Dolby Vision, WebDAV/DLNA/SMB streaming, dual subtitles, and browser remote control"
   homepage "https://github.com/FengZeng/soia"
 
   app "Soia.app"
